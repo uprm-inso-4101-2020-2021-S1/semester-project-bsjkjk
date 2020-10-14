375abe3a2aafa8d53c29ea2fc5cf48610ecf750c
Jose J. Vazquez Perez <jose.vazquez38@upr.edu>
2020-10-14 12:26:21 -0400

    fixing database. Note: might need to use anoter type of database.

--- CFRProject/app.py
+++ CFRProject/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cfrdb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cfr.db'
 db = SQLAlchemy(app)
 
 
