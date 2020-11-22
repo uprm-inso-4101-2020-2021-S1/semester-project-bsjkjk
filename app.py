a5bd26c056001c2cf3c5066cb0a0f61fe20ec600
josejavi3009 <jose.vazquez38@upr.edu>
2020-11-22 00:01:32 -0400

    The app now adds an account (email, username, and password) to the database. (SignUp page). Added an Admin, ModelView and ModelIndexView classes, but need further testing.

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
 from datetime import datetime
 
 app = Flask(__name__)
+
+#variable DEV used for testing: it uses a sqlite dabatase to test, else: it uses heroku postgresql's database
 DEV = False
 
 if DEV == False:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://spfuntodxqvpaf:89bed639820cfd6b6a171efc9574fe88604059843315341a305f61d17e581e63@ec2-107-20-15-85.compute-1.amazonaws.com:5432/d8lljt8rmrco0d'
+    app.config['SECRET_KEY'] = 'somesecretkey'
+
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cfr.db'
+    app.config['SECRET_KEY'] = 'somesecretkey'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
-
+@login.user_loader
+def load_user(user_id):
+    return Accounts.query.get(user_id)
 
 ## table used for storing fault reports ##
 class Report(db.Model):
@@ -33,7 +44,7 @@ class Report(db.Model):
         return '<Fault Report %r>' % self.id
 
 ## table used for storing account information ##
-class Accounts(db.Model):
+class Accounts(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True)
     account_username = db.Column(db.String(15), unique=True, nullable=False)
     account_email = db.Column(db.String(100), unique=True, nullable=False)
@@ -42,9 +53,24 @@ class Accounts(db.Model):
     def __repr__(self, account_username, account_email, password):
         self.account_username = account_username
         self.account_email = account_email
-        sel.password = password
+        self.password = password
         return '<Account %r>' % self.user_id
+##### Account and admin stuff ########################
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
 
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('signIn'))
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+admin = Admin(app, index_view=MyAdminIndexView)
+admin.add_view(MyModelView(Accounts, db.session))
+
+##########################################################
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -85,14 +111,37 @@ def allReports():
         reports = reversed(reports)
         return render_template("allReports.html", reports=reports)
 
-###################Sign Up/Sign In routes####################################
-@app.route('/signUp')
+###################Sign Up/Sign In routes (and LogOut)####################################
+@app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        form  = request.form
+        for field in form:
+            if form[field] == "":
+                return redirect('/SignUp')
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        new_account = Accounts(account_username=username, account_email=email, password=password)
+        try:
+            db.session.add(new_account)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem creating new account.'
+    else:
+        return render_template("signup.html")
 
-@app.route('/signIn')
-def signIn():
+@app.route('/signIn/<int:id>')
+def signIn(id):
+    user = Accounts.query.get_or_404(id)
+    login_user(user)
     return render_template("signIn.html")
+
+@app.route('/logOut')
+def logOut():
+    logout_user()
+    return redirect('/signIn')
 ##############################################################################
 
 @app.route('/delete/<int:id>')
