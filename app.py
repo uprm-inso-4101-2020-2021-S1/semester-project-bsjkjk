4c98100d0c9020e9f942bbf7b497e938fe361eb4
josejavi3009 <jose.vazquez38@upr.edu>
2020-11-23 14:25:42 -0400

    omg DEV mode was on again

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 
 app = Flask(__name__)
 #variable DEV used for testing: it uses a sqlite dabatase to test, else: it uses heroku postgresql's database
-DEV = True
+DEV = False
 
 if DEV == False:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
