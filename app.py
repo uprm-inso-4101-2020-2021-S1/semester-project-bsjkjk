be98282cd1be8cf30b9b63e83a834af5be67cd92
josejavi3009 <jose.vazquez38@upr.edu>
2020-11-22 21:46:51 -0400

    oops, pushed with DEV mode on, pushing with DEV mode off

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 #variable DEV used for testing: it uses a sqlite dabatase to test, else: it uses heroku postgresql's database
-DEV = True
+DEV = False
 
 if DEV == False:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
